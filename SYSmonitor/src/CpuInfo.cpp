7e2a7676ccc304b73e042a50d8e816ceab425c07
AngryOwl64 <angryowl64@proton.me>
2026-05-21 17:13:50 +0200

    cpu calc begin

--- SYSmonitor/src/CpuInfo.cpp
+++ SYSmonitor/src/CpuInfo.cpp
@@ -5,3 +5,10 @@ int main() {
 }
 
 float calculateCpuLoad()
+
+float GetCpuTimes() {
+	FILETIME idleTime, kernelTime, userTime;
+	GetSystemTimes(&idleTime, &kernelTime, &userTime);
+	auto var = (static_cast<ULONGLONG>(idleTime.dwHighDateTime) << 32 ) | idleTime.dwLowDateTime;
+
+}

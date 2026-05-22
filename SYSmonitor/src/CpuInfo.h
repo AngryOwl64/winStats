5f26422b08622fed941230272e396742b368b113
AngryOwl64 <angryowl64@proton.me>
2026-05-22 15:28:04 +0200

    cpu calc finished + runtime

--- SYSmonitor/src/CpuInfo.h
+++ SYSmonitor/src/CpuInfo.h
@@ -2,3 +2,14 @@
 #include <iostream>
 #include <iomanip>
 #include <windows.h>
+
+struct CpuSnapshot {
+	ULONGLONG idle;
+	ULONGLONG kernel;
+	ULONGLONG user;
+};
+
+namespace CpuCalc {
+	CpuSnapshot GetCpuTimes();
+	double calculateCpuUsage(CpuSnapshot old, CpuSnapshot newer);
+}
